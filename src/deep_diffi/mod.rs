f482bfbe4cc0c0d429d10f0e9c6bba1ecf74ec68
Sharif Noor <sharifnoor742743284@gmail.com>
2022-10-20 16:33:23 +0500

    Added Fixed Order functionalities

--- src/deep_diffi/mod.rs
+++ src/deep_diffi/mod.rs
@@ -1,6 +1,6 @@
 pub mod mod_deep_diffi {
 
-    use serde::Serialize;
+    use serde::{Serialize};
     use serde_derive::{Deserialize, Serialize};
     use serde_json::{json, Value};
 
@@ -57,8 +57,15 @@ pub mod mod_deep_diffi {
     static mut ITERABLE_ITEM_ADDED: Vec<Value> = Vec::new();
     static mut ITERABLE_ITEM_REMOVED: Vec<Value> = Vec::new();
 
-    pub fn josn_diffi(a_json: serde_json::Value, b_json: serde_json::Value) -> Outer {
-        deep_diffi(a_json, b_json, "root[".to_string());
+    pub fn josn_diffi(a_json: serde_json::Value, b_json: serde_json::Value, order: bool) -> Outer {
+
+        if !order {
+            deep_diffi(a_json, b_json, "root[".to_string());
+        }
+        else {
+            deep_diffi_order(a_json, b_json, "root[".to_string());
+        }
+        
         let dictionary_item_removed: Bar;
         let dictionary_item_added: Bar5;
         let value_changed: Bar3;
@@ -99,29 +106,29 @@ pub mod mod_deep_diffi {
         
     }
 
-    fn deep_diffi(a_json: serde_json::Value, b_json: serde_json::Value, init_location: String) {
+    fn deep_diffi(a_json: serde_json::Value, b_json: serde_json::Value, _init_location: String) {
         let json1: &serde_json::Map<String, serde_json::Value> = a_json.as_object().unwrap();
         let json2: &serde_json::Map<String, serde_json::Value> = b_json.as_object().unwrap();
-        let mut location: String;
-        for (json1_key, json1_value) in json1.iter() {
-            location = init_location.to_owned() + &json1_key + "]";
+        let mut _location: String;
+        for (json1_key, _json1_value) in json1.iter() {
+            _location = _init_location.to_owned() + &json1_key + "]";
             if !json2.contains_key(json1_key) {
                 // dictionary_item_removed
-                let val = json1_value.to_owned();
-                let formatted = json!({"location": location, "value": val});
+                let val = _json1_value.to_owned();
+                let formatted = json!({"location": _location, "value": val});
                 unsafe {
                     DICTIONARY_ITEM_REMOVED_ARRAY.push(formatted);
                 }
             } else {
                 // JSON-2 contains Key-1
-                if json1_value.is_object() {
+                if _json1_value.is_object() {
                     // Recursion
-                    let json3 = json1_value;
+                    let json3 = _json1_value;
                     let json4 = json2[json1_key].as_object().unwrap();
-                    deep_diffi(json!(json3), json!(json4), location.to_string());
-                } else if json1_value.is_array() {
+                    deep_diffi(json!(json3), json!(json4), String::from(&_location) + "[");
+                } else if _json1_value.is_array() {
                     if json2[json1_key].is_array() {
-                        for json1_array_val in json1_value.as_array().unwrap().iter() {
+                        for json1_array_val in _json1_value.as_array().unwrap().iter() {
                             if !json2[json1_key]
                                 .as_array()
                                 .unwrap()
@@ -129,23 +136,23 @@ pub mod mod_deep_diffi {
                             {
                                 // Iterable Item Removed
                                 let val = json1_array_val.to_owned();
-                                let formatted = json!({"location": location, "value": val});
+                                let formatted = json!({"location": _location, "value": val});
                                 unsafe {
                                     ITERABLE_ITEM_REMOVED.push(formatted);
                                 }
                             }
                             else if json1_array_val.is_object() {
-                                let j1i = json1_value.as_array().unwrap().iter().position(|x| x == json1_array_val).unwrap();
-                                let location1 = String::from(&location) + "[" + &j1i.to_string() + "][";
+                                let j1i = _json1_value.as_array().unwrap().iter().position(|x| x == json1_array_val).unwrap();
+                                let location1 = String::from(&_location) + "[" + &j1i.to_string() + "][";
                                 let arr_1 = json1_array_val;
                                 deep_diffi(json!(&arr_1), json!(&json2[json1_key][j1i]), location1.to_string());
                             }
                         }
                         for json2_array_val in json2[json1_key].as_array().unwrap().iter() {
-                            if !json1_value.as_array().unwrap().contains(json2_array_val) && !json2_array_val.is_object() {
+                            if !_json1_value.as_array().unwrap().contains(json2_array_val) && !json2_array_val.is_object() {
                                 // Iterable Item Added
                                 let val = json2_array_val.to_owned();
-                                let formatted = json!({"location": location, "value": val});
+                                let formatted = json!({"location": _location, "value": val});
                                 unsafe {
                                     ITERABLE_ITEM_ADDED.push(formatted);
                                 }
@@ -154,10 +161,10 @@ pub mod mod_deep_diffi {
                     }
                 } else {
                     // JSON-2 contains Key-1 && Value is not an Object or Array
-                    if json1_value != &json2[json1_key] {
+                    if _json1_value != &json2[json1_key] {
                         // value_changed
-                        let val = json1_value.to_owned();
-                        let formatted = json!({"location": location, "value": val});
+                        let val = _json1_value.to_owned();
+                        let formatted = json!({"location": _location, "value": val});
                         unsafe {
                             VALUE_CHANGED_ARRAY.push(formatted);
                         }
@@ -165,15 +172,77 @@ pub mod mod_deep_diffi {
                 }
             }
         }
-        for (json2_key, json2_value) in json2 {
-            location = init_location.to_owned() + &json2_key + "]";
+        for (json2_key, _json2_value) in json2 {
+            _location = _init_location.to_owned() + &json2_key + "]";
             if !json1.contains_key(json2_key) {
                 // dictionary_item_added
-                let val = json2_value.to_owned();
-                let formatted = json!({"location": location, "value": val});
+                let val = _json2_value.to_owned();
+                let formatted = json!({"location": _location, "value": val});
                 unsafe {
                     DICTIONARY_ITEM_ADDED_ARRAY.push(formatted);
                 }
+            }
+        }
+    }
+
+    fn deep_diffi_order(a_json: serde_json::Value, b_json: serde_json::Value, _init_location: String) {
+        let json1: &serde_json::Map<String, serde_json::Value> = a_json.as_object().unwrap();
+        let json2: &serde_json::Map<String, serde_json::Value> = b_json.as_object().unwrap();
+        let mut _location: String;
+
+        for (json1_i, (json1_key, _json1_value)) in json1.iter().enumerate() {
+            _location = _init_location.to_owned() + &json1_key + "]";
+            let json2_key = json2.iter().nth(json1_i);
+
+            if json2_key.is_some() {
+                if json1_key == json2_key.unwrap().0 {
+                    if !_json1_value.is_object() && !_json1_value.is_array(){
+                        if _json1_value != json2_key.unwrap().1 {
+                            let val = _json1_value.to_owned();
+                            let formatted = json!({"location": _location, "value": val});
+                            unsafe {
+                                VALUE_CHANGED_ARRAY.push(formatted);
+                            }
+                        }
+                    }
+                    else if _json1_value.is_object() {
+                        let json3 = _json1_value;
+                        let json4 = json2[json1_key].as_object().unwrap();
+                        deep_diffi_order(json!(json3), json!(json4), String::from(&_location) + "[");
+                    }
+                    else if _json1_value.is_array() {
+                        if json2[json1_key].is_array() {
+                            for (json1_array_val_i, json1_array_val) in _json1_value.as_array().unwrap().iter().enumerate() {
+                                let json4_key = &json2[json1_key][json1_array_val_i];
+                                
+                                if json1_array_val.is_object() {
+                                    let j1i = _json1_value.as_array().unwrap().iter().position(|x| x == json1_array_val).unwrap();
+                                    let location1 = String::from(&_location) + "[" + &j1i.to_string() + "][";
+                                    let arr_1 = json1_array_val;
+                                    deep_diffi_order(json!(&arr_1), json!(&json2[json1_key][j1i]), location1.to_string());
+                                }
+                                else {
+                                    if json1_array_val != json4_key {
+                                        let j1i = _json1_value.as_array().unwrap().iter().position(|x| x == json1_array_val).unwrap();
+                                        let location1 = String::from(&_location) + "[" + &j1i.to_string() + "]";
+                                        let val = json1_array_val.to_owned();
+                                        let formatted = json!({"location": location1, "value": val});
+                                        unsafe {
+                                            VALUE_CHANGED_ARRAY.push(formatted);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            else {
+                println!("===> Order Compromised!");
+                unsafe {
+                    VALUE_CHANGED_ARRAY.clear();
+                }
+                deep_diffi(json!(a_json), json!(b_json), "root[".to_string());
             }
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 mod deep_diffi;
-use serde_json::json;
 use deep_diffi::mod_deep_diffi::josn_diffi;
+use serde_json::json;
 
 fn main() {
-
     let a_json = json!({
         "key": "value_1",
         "key2": {
@@ -11,15 +10,28 @@ fn main() {
             "key2": [
                 "value_3",
                 "value_4",
+                "value_6",
             ],
             "key3": "value_2",
         },
-        "key4": [{"keyj4": "valuej_222","keyj5": "valuej_2"}, {"keyj42": "valuej_22","keyj52": "valuej_22","keyj62": "valuej_22"}],
+        "key4": [
+            {
+                "keyj4": "valuej_222",
+                "keyj5": "valuej_2",
+            },
+            {
+                "keyj42": "valuej_22",
+                "keyj52": "valuej_22",
+                "keyj62": "valuej_22",
+            }
+        ],
+        "key5": "value_1",
     });
 
     let b_json = json!({
+        "key": "value_1",
         "key2": {
-            "key": "value_2",
+            "key_2": "value_4",
             "key2": [
                 "value_3",
                 "value_5",
@@ -27,9 +39,20 @@ fn main() {
             ],
             "key3": "value_3",
         },
-        "key4": [{"keyj4": "valuej_2","keyj5": "valuej_2","keyj62": "valuej_22"}, {"keyj42": "valuej_22","keyj52": "valuej_22"}],
+        "key4": [
+            {
+                "keyj4": "valuej_2",
+                "keyj5": "valuej_2",
+                "keyj62": "valuej_22",
+            }, 
+            {
+                "keyj42": "valuej_22",
+                "keyj52": "valuej_22",
+            }
+        ],
+        "key5": "value_2",
     });
 
-    let o = josn_diffi(a_json, b_json);
+    let o = josn_diffi(a_json, b_json, true);
     println!("===> {}", serde_json::to_string(&o).unwrap());
 }
